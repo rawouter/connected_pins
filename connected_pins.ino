298e9546d938910113342e1c0c267b58e2feefbd
Raphael Wouters <rawouter@cisco.com>
2016-09-27 10:41:55 +0200

    Added two demo modes

--- connected_pins.ino
+++ connected_pins.ino
@@ -1,6 +1,8 @@
 #include <Adafruit_NeoPixel.h>
 #include <math.h>
 
+#define MODE_RAINBOW
+#define MODE_SERIAL
 #define PIN 12
 #define BREATH_TIME 3000 // MilliSecond
 
@@ -13,18 +15,17 @@ void setup() {
   strip.begin();
   set_color(strip.Color(0, 0, 255));
   strip.show(); // Initialize all pixels to 'off'
+
+#ifdef MODE_RAINBOW
+  rainbow_demo();
+#endif
 }
 
 void loop() {
-  char c = Serial.read();
-  if (c=='r') {set_color(strip.Color(255,0,0));} 
-  if (c=='g') {set_color(strip.Color(0,255,0));}
-  if (c=='b') {set_color(strip.Color(0,0,255));} 
-  if (c=='y') {set_color(strip.Color(255,255,0));}
-  if (c=='m') {set_color(strip.Color(255,0,255));} 
-  if (c=='c') {set_color(strip.Color(0,255,255));}
-  if (c=='w') {set_color(strip.Color(255,255,255));}
-  
+#ifdef MODE_SERIAL
+    set_color_from_serial();
+#endif
+
   // Redraw pixel with brightness
   float bright = get_brightness();
   strip.setBrightness(bright);
@@ -44,6 +45,55 @@ float get_brightness() {
   if (now > BREATH_TIME) {
     return 0;
   } else {
+    Serial.println(now);
     return (exp(sin(now/2000.0*PI)) - 0.36787944)*108.0;
   }
 }
+
+#ifdef MODE_SERIAL
+void set_color_from_serial() {
+  if (Serial.available() > 0) {
+    char c = Serial.read();
+    switch(c) {
+      case 'o': set_color(strip.Color(0,0,0)); break;
+      case 'r': set_color(strip.Color(255,0,0)); break;
+      case 'g': set_color(strip.Color(0,255,0)); break;
+      case 'b': set_color(strip.Color(0,0,255)); break;
+      case 'y': set_color(strip.Color(255,255,0)); break;
+      case 'm': set_color(strip.Color(255,0,255)); break;
+      case 'c': set_color(strip.Color(0,255,255)); break;
+      case 'w': set_color(strip.Color(255,255,255)); break;
+    }
+  }
+}
+#endif
+
+#ifdef MODE_RAINBOW
+void rainbow_demo() {
+  const int DELAY = 2000;
+  set_color(strip.Color(255,51,153)); // emergency
+  strip.show();
+  delay(DELAY);
+  set_color(strip.Color(255,0,0)); // alert
+  strip.show();
+  delay(DELAY);
+  set_color(strip.Color(255,80,80)); // critical
+  strip.show();
+  delay(DELAY);
+  set_color(strip.Color(255,153,51)); // error
+  strip.show();
+  delay(DELAY);
+  set_color(strip.Color(255,255,0)); // warning
+  strip.show();
+  delay(DELAY);
+  set_color(strip.Color(51,153,255)); // notice
+  strip.show();
+  delay(DELAY);
+  set_color(strip.Color(111,252,182)); // info
+  strip.show();
+  delay(DELAY);
+  set_color(strip.Color(162,250,150)); // debug
+  strip.show();
+  delay(500);
+}
+#endif
